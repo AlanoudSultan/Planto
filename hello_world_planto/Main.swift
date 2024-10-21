4a3abf67eccea9cb80e581e3ef46e8a79b594966
Alanoud <abaalkhailalanoud@gmail.com>
2024-10-21 14:56:15 +0300

    test

---
+++ hello_world_planto/Main.swift
@@ -0,0 +1,18 @@
+//
+//  Main.swift
+//  hello_world_planto
+//
+//  Created by Alanoud Abaalkhail on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+struct Main: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Main()
+}
