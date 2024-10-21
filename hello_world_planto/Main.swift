08cd10b40b983ff07f9220349be26b6f5ae96a22
Alanoud <abaalkhailalanoud@gmail.com>
2024-10-21 17:45:12 +0300

    Main Page

--- hello_world_planto/Main.swift
+++ hello_world_planto/Main.swift
@@ -9,7 +9,48 @@ import SwiftUI
 
 struct Main: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color.black.ignoresSafeArea()
+            VStack(alignment: .leading){
+                Text("My Plants 🌱 ")
+                    .font(.system(size: 34))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Divider()
+                    .background(Color.gray)
+                     Spacer()
+                Image("plant")
+                    .resizable()
+                    .frame(width: 164.0,height: 200.0)
+                    .position(x:200 , y:140)
+                   
+                
+                Text("Start your plant journey! ")
+                    .foregroundColor(Color("whitish"))
+                    .font(.system(size: 25))
+                    .fontWeight(.semibold)
+                    .position(x:200 , y: 130)
+                
+                Text("Now all your plants will be in one place and we will help you take care of them :)🪴")
+                    .foregroundStyle(Color("grayish"))
+                    .position(x:200 , y: 14)
+                
+                Button("Set Plant Reminder") {
+                   
+                }
+                .frame(width: 280 , height: 40)
+                .foregroundColor(Color("blacks"))
+                .background(Color("green"))
+                .cornerRadius(10)
+                .position(x:200 , y: -100)
+                
+
+                    
+                
+            }
+            
+        }
     }
 }
 
---
+++ hello_world_planto/Plantapp.swift
@@ -0,0 +1,16 @@
+//
+//  Plantapp.swift
+//  hello_world_planto
+//
+//  Created by Alanoud Abaalkhail on 18/04/1446 AH.
+//
+
+import SwiftUI
+@main
+struct hello_world_planto9999App: App {
+    var body: some Scene {
+        WindowGroup {
+            Main()
+        }
+    }
+}
