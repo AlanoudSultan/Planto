61a00c5d0e177b27aafdbb8c50db6c0f5bac5bfb
Alanoud <abaalkhailalanoud@gmail.com>
2024-10-21 14:34:29 +0300

    Initial Commit

---
+++ hello_world_planto/hello_world_plantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_world_plantoApp.swift
+//  hello_world_planto
+//
+//  Created by Alanoud Abaalkhail on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct hello_world_plantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
